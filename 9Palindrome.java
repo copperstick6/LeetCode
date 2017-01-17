706b675d54ba03831a7b12e06964cf47071dfda1
William Han <han818william@hotmail.com>
2017-01-17 15:28:02 -0600

    Fixed! Final solution accepted. Runtime: 199ms
    80.39% faster than most solutions.

--- 9Palindrome.java
+++ 9Palindrome.java
@@ -1,12 +1,22 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        Stack<Integer> stackofInts = new Stack<Integer>();
-        Stack<Integer> secondStack = new Stack<Integer>();
-        int num = x;
-        boolean result = true;
-        while(curNum > 0)
+        String result = Integer.toString(x);
+        boolean found = true;
+        int curPos = 0;
+        int end = result.length() - 1;
+        int middle = result.length()/2;
+        while(found && curPos <= middle)
         {
-            
+            char beginningLetter = result.charAt(curPos);
+            char endLetter = result.charAt(end);
+            if(!(beginningLetter == endLetter))
+            {
+                found = false;
+                return found;
+            }
+            curPos++;
+            end--;
         }
+        return found;
     }
 }
