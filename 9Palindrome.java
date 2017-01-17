64e4f2302a83118a7a2b0acdaf3bf27cc51b353e
William Han <han818william@hotmail.com>
2017-01-17 13:47:38 -0600

    Create 9Palindrome.java

---
+++ 9Palindrome.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        Stack<Integer> stackofInts = new Stack<Integer>();
+        Stack<Integer> secondStack = new Stack<Integer>();
+        int num = x;
+        boolean result = true;
+        while(curNum > 0)
+        {
+            
+        }
+    }
+}
